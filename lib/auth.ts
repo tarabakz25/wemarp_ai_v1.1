1dee9bdc2e897a9a6a59b224c70e37f49afe385b
Kizuki <aikikizuki@gmail.com>
2025-10-03 19:28:09 +0900

    feat: make certification

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,41 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/lib/prisma";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [ GitHub ],
-});
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
 
-
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID! as string,
+      clientSecret: process.env.GITHUB_SECRET! as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID! as string,
+      clientSecret: process.env.GOOGLE_SECRET! as string,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+} satisfies Parameter<typeof NextAuth>[0];
